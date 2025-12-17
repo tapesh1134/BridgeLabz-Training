5f148baaf97f3d2911dbb94336e313feb6dd0d63
tapesh1134 <tapeshs1134@gmail.com>
2025-12-17 21:21:48 +0530

    [tapesh] Add.Perimeter calculation program for a rectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/perimeter.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class perimeter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of rectangle: ");
+        int length = sc.nextInt();
+        System.out.print("Enter breadth of rectangle: ");
+        int breadth = sc.nextInt();
+        System.out.print("Perimeter of rectangle: " + 2 * (length + breadth));
+        sc.close();
+    }
+}
