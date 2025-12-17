dc55c86ff9a17fe0fc9f6038bab2f38edfaeeeaa
tapesh1134 <tapeshs1134@gmail.com>
2025-12-17 21:25:19 +0530

    [tapesh] Add.Power calculation program using base and exponent

---
+++ core-java-practice/gcr-codebase/programming-elements/power.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class power {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base number: ");
+        int base = sc.nextInt();
+        System.out.print("Enter exponent: ");
+        int exponent = sc.nextInt();
+        System.out.println("Result: " + Math.pow(base, exponent));
+        sc.close();
+    }
+}
