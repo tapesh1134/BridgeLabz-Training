0e416e76c8b8bc6d5766874aee608b5bdcfb8be8
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 15:07:17 +0530

    [tapesh] Add.Operations program with user int inputs

---
+++ core-java-practice/gcr-codebase/programming-elements/intoperation.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class intoperation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        System.out.printf("Operations:\n" + "a + b * c = %d\n" + "a * b + c = %d\n" + "c + a / b = %d\n" + "a %% b + c = %d\n", (num1 + num2 * num3), (num1 * num2 + num3), (num3 + num2 / num1), (num1 % num2 * num3));
+        sc.close();
+    }
+}
