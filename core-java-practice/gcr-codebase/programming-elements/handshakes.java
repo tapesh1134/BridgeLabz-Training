3280ed4e27e48dda8fb4504a9d947ffaf292157f
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 14:54:46 +0530

    [tapesh] Add.Handshake calculation program based on the number of students

---
+++ core-java-practice/gcr-codebase/programming-elements/handshakes.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class handshakes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter total number of students: ");
+        int students = sc.nextInt();
+        System.out.printf("Number of possible handshakes %d", (students * (students + 1) / 2));
+        sc.close();
+    }
+}
