c411ae946cbbfb6a1104abb3c515bcbd396e6af7
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 11:06:47 +0530

    [tapesh] Add.Leetcode remove elements from array program

---
+++ core-java-practice/leet-code-codebase/removeelements.java
@@ -0,0 +1,24 @@
+
+public class removeelements {
+
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+        System.out.print("Nums: ");
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+}
