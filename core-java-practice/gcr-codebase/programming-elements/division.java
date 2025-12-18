840e3c9a0a3a0df4ba9f5f3a9bd42af0e38e36c4
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 14:58:26 +0530

    [tapesh] Add.Division program to calculate quotient and remainder of two numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/division.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class division {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.printf("The Quotient is %d and Reminder is %d of two number %d and %d", num1 / num2, num1 % num2, num1, num2);
+        sc.close();
+    }
+}
