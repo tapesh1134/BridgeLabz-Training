c8027075b968f80ba6068f3a9ee6c94a2325db3b
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 14:32:06 +0530

    [tapesh] Add.Basic calculator program for addition, subtraction, multiplication and division

---
+++ core-java-practice/gcr-codebase/programming-elements/calculator.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.printf("The addition, subtraction, multiplication and division value of 2 numbers %d and %d is %d, %d, %d, and %.2f", num1, num2, num1 + num2, num1 - num2, num1 * num2, (double) num1 / num2);
+        sc.close();
+    }
+}
