bf21787d693cd6b0c3860a0dc64937d59f5ac28e
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 11:50:32 +0530

    [tapesh] Add.University discount calculation program

---
+++ core-java-practice/gcr-codebase/programming-elements/discount.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class discount {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the fees of university: ");
+        int fees = sc.nextInt();
+        System.out.print("Enter the discount: ");
+        int discountPercentage = sc.nextInt();
+        System.out.printf("The fees you have to pay by subtracting the discount from the fee: %.2f", (double) fees - fees / discountPercentage);
+        sc.close();
+    }
+}
