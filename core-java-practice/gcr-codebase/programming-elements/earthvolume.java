a758ebda7f4b7ff5d4172bc75984fa3adc42b49a
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 12:08:22 +0530

    [tapesh] Add.Earth volume calculation program in km and miles

---
+++ core-java-practice/gcr-codebase/programming-elements/earthvolume.java
@@ -0,0 +1,8 @@
+
+public class earthvolume {
+
+    public static void main(String[] args) {
+        int radius = 6378;
+        System.out.printf("The volume of earth in cubic kilometers is %.2f and cubic meters is %.2f", (double) (4 / 3) * Math.PI * Math.pow(radius, 3), (double) (4 / 3) * Math.PI * Math.pow(radius * 0.621371, 3));
+    }
+}
