74f78fecd0854dc4e5727ca5cb26b45bee1b483c
tapesh1134 <tapeshs1134@gmail.com>
2025-12-18 14:46:37 +0530

    [tapesh] Add.Distance conversion program from yards to miles and feet

---
+++ core-java-practice/gcr-codebase/programming-elements/distanceconversion.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class distanceconversion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the distance in yards: ");
+        int yards = sc.nextInt();
+        System.out.printf("Distance in yards %d, distance in miles %.2f, distance in feet %d", yards, (double) yards / 1760, 3 * yards);
+        sc.close();
+    }
+}
