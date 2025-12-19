161e0f7569cf3fe62c8d1d9ca10766e6735c12fa
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:38:06 +0530

    [tapesh] Refactor. Fixing naming convention of finding quotient and remainder program

--- core-java-practice/gcr-codebase/programming-elements/QuotientAndRemainder.java
+++ core-java-practice/gcr-codebase/programming-elements/QuotientAndRemainder.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class division {
+public class QuotientAndRemainder {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
