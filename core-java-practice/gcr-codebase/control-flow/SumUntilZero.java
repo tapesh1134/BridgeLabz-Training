0620da9698fa3527aa7d5d48f5b3e8aecf253336
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 16:52:00 +0530

    [tapesh] Refactor. Fixing data type of total and input from int to double

--- core-java-practice/gcr-codebase/control-flow/SumUntilZero.java
+++ core-java-practice/gcr-codebase/control-flow/SumUntilZero.java
@@ -4,16 +4,16 @@ public class SumUntilZero {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		double sum = 0.0;
+		double total = 0.0;
 		while (true) {
 			System.out.print("Enter a number and enter zero for stop: ");
 			double input = scanner.nextDouble();
-			sum += input;
+			total += input;
 			if (input == 0) {
 				break;
 			}
 		}
-		System.out.printf("Total sum: %.2f", sum);
+		System.out.printf("Total sum: %.2f", total);
 		scanner.close();
 	}
 
