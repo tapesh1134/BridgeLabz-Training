43adf45ec8b408d515852206e662fa0ce5bd548d
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:04:58 +0530

    [tapesh] Refactor. Fix naming convention for university discount program

--- core-java-practice/gcr-codebase/programming-elements/UniversityDiscount.java
+++ core-java-practice/gcr-codebase/programming-elements/UniversityDiscount.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class discount {
+public class UniversityDiscount {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
