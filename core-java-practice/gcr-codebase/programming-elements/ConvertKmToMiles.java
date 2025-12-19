00795dbc37f62e9507f83f0d3cfad9a45f94e62f
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:27:53 +0530

    [tapesh] Refactor. Fixing naming convention of km to miles program

--- core-java-practice/gcr-codebase/programming-elements/ConvertKmToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/ConvertKmToMiles.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class kmtomiles {
+public class ConvertKmToMiles {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
