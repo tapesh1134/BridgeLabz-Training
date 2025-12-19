739a5f79b2a6e0d4d72b336e3466b9c296a7cfb0
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:13:11 +0530

    [tapesh] Refactor. Fix naming convention for calculating possible number of handshakes

--- core-java-practice/gcr-codebase/programming-elements/PossibleHandshakes.java
+++ core-java-practice/gcr-codebase/programming-elements/PossibleHandshakes.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class handshakes {
+public class PossibleHandshakes {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
