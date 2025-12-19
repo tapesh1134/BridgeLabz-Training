7d6c81aaa18c8c67d6eaaad28cf35cf804d15164
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 22:25:01 +0530

    [tapesh] Refactor. Fixing conditions of leap year program

--- core-java-practice/gcr-codebase/control-flow/LeapYear.java
+++ core-java-practice/gcr-codebase/control-flow/LeapYear.java
@@ -5,14 +5,18 @@ public class LeapYear {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a year: ");
 		int year = scanner.nextInt();
-		if (year >= 1582) {
-			if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)) {
-				System.out.printf("%d is a leap year", year);
-			} else {
-				System.out.printf("%d is not a leap year", year);
-			}
+		if (year < 1582) {
+			System.out.println("Year must be 1582 or later (Gregorian calendar)");
 		} else {
-			System.out.println("Enter a valid year");
+			if (year % 400 == 0) {
+				System.out.println("Year is a Leap Year");
+			} else if (year % 100 == 0) {
+				System.out.println("Year is not a Leap Year");
+			} else if (year % 4 == 0) {
+				System.out.println("Year is a Leap Year");
+			} else {
+				System.out.println("Year is not a Leap Year");
+			}
 		}
 		scanner.close();
 	}
