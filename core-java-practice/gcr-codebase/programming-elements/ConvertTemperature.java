3292432930603a414228bfebb1cceda9a9f7fb44
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:22:38 +0530

    [tapesh] Refactor. Fixing naming convention of convert temperature program

--- core-java-practice/gcr-codebase/programming-elements/ConvertTemperature.java
+++ core-java-practice/gcr-codebase/programming-elements/ConvertTemperature.java
@@ -1,15 +1,14 @@
 
 import java.util.*;
 
-public class temperatureconversion {
+public class ConvertTemperature {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter temperature in degree:");
         // Formula: (0°C × 9/5) + 32 = 32°F
         int temperature = sc.nextInt();
-        int converted = (temperature * (9 / 5)) + 32;
-        System.out.println("After conversion: " + converted);
+        System.out.println("After conversion: " + (temperature * (9 / 5)) + 32);
         sc.close();
     }
 }
