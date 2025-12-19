f3035032ec965022d7dcfc22c798aba9370039ce
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:03:38 +0530

    [tapesh] Refactor. Fix naming convention for calculator program

--- core-java-practice/gcr-codebase/programming-elements/BasicCalculator.java
+++ core-java-practice/gcr-codebase/programming-elements/BasicCalculator.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class calculator {
+public class BasicCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
