d1ab8e52d3710f637ae185b967bbe1b4cba8fb61
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:35:17 +0530

    [tapesh] Refactor. Fixing naming convention of perimeter of square program

--- core-java-practice/gcr-codebase/programming-elements/PerimeterOfSquare.java
+++ core-java-practice/gcr-codebase/programming-elements/PerimeterOfSquare.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class squareperimeter {
+public class PerimeterOfSquare {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
