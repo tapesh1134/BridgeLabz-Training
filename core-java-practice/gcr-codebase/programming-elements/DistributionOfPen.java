ee8fe0b4f4a4146580383657290b704d3067d368
tapesh1134 <tapeshs1134@gmail.com>
2025-12-19 11:21:32 +0530

    [tapesh] Refactor. Fixing naming convention of pen distribution program

--- core-java-practice/gcr-codebase/programming-elements/DistributionOfPen.java
+++ core-java-practice/gcr-codebase/programming-elements/DistributionOfPen.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class pendistribution {
+public class DistributionOfPen {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
