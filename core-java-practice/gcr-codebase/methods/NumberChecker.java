55c7e2fc118b09eb7a2afa9f77d88173956cdd0f
tapesh1134 <tapeshs1134@gmail.com>
2025-12-23 11:41:15 +0530

    [tapesh] Add, Program to create a NumberChecker utility class

---
+++ core-java-practice/gcr-codebase/methods/NumberChecker.java
@@ -0,0 +1,76 @@
+
+public class NumberChecker {
+
+    public static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    public static int[] storeDigits(int number) {
+        int count = countDigits(number);
+        int[] digits = new int[count];
+
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    public static boolean isDuckNumber(int[] digits) {
+        for (int digit : digits) {
+            if (digit != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isArmstrongNumber(int number, int[] digits) {
+        int sum = 0;
+        int power = digits.length;
+
+        for (int digit : digits) {
+            sum += Math.pow(digit, power);
+        }
+        return sum == number;
+    }
+
+    public static void findLargestAndSecondLargest(int[] digits) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int digit : digits) {
+            if (digit > largest) {
+                secondLargest = largest;
+                largest = digit;
+            } else if (digit > secondLargest && digit != largest) {
+                secondLargest = digit;
+            }
+        }
+
+        System.out.println("Largest Digit: " + largest);
+        System.out.println("Second Largest Digit: " + secondLargest);
+    }
+
+    public static void findSmallestAndSecondSmallest(int[] digits) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int digit : digits) {
+            if (digit < smallest) {
+                secondSmallest = smallest;
+                smallest = digit;
+            } else if (digit < secondSmallest && digit != smallest) {
+                secondSmallest = digit;
+            }
+        }
+
+        System.out.println("Smallest Digit: " + smallest);
+        System.out.println("Second Smallest Digit: " + secondSmallest);
+    }
+}
