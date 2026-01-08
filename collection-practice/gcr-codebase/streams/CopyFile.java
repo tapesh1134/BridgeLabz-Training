72e461f0a9c025637c5f19761a6900b7d7a63c1e
tapesh1134 <tapeshs1134@gmail.com>
2026-01-08 11:10:44 +0530

    [tapesh] Refactor. Fixing program for copy file using FileInputStream and FileOutputStream

--- collection-practice/gcr-codebase/streams/CopyFile.java
+++ collection-practice/gcr-codebase/streams/CopyFile.java
@@ -1,23 +1,38 @@
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class CopyFile {
 
     public static void main(String[] args) {
-        String mainFile = "main.txt";
-        String newFile = "duplicate.txt";
-        try (
-                FileInputStream fis = new FileInputStream(mainFile); FileOutputStream fos = new FileOutputStream(newFile)) {
+        File input = new File("input.txt");
+        File output = new File("ouput.txt");
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            if (!input.exists()) {
+                System.out.println("Source file does not exist.");
+                return;
+            }
+            fis = new FileInputStream(input);
+            fos = new FileOutputStream(output);
             int data;
             while ((data = fis.read()) != -1) {
                 fos.write(data);
             }
-            System.out.println("Copy successfully done");
-
+            System.out.println("File copied successfully");
         } catch (IOException e) {
-            System.out.println("Main file not found");
+            System.out.println("Error while reading or writing file");
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error while closing file");
+            }
         }
     }
 }
