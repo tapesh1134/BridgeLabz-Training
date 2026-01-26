3f7876749a6efa3c957bc399edac8b902b2f7304
tapesh1134 <tapeshs1134@gmail.com>
2026-01-26 12:24:30 +0530

    [tapesh] Add. Implement AddressBook class with methods to add and retrieve contacts

---
+++ oops-practice/gcr-codebase/address-book/src/main/java/org/tapesh/dao/AddressBook.java
@@ -0,0 +1,24 @@
+package org.tapesh.dao;
+
+
+import org.tapesh.modal.Contact;
+
+import java.util.HashMap;
+
+public class AddressBook {
+    HashMap<String, Contact> contactLibrary;
+    public AddressBook(){
+        this.contactLibrary = new HashMap<>();
+    }
+
+    public void addContact(Contact contact) {
+        String key = contact.getFirstName() + " " + contact.getLastName();
+        contactLibrary.put(key.toLowerCase(),contact);
+    }
+
+    public Contact getContact(String inputName) {
+        System.out.println(inputName);
+        Contact contact = contactLibrary.get(inputName.toLowerCase());
+        return contact;
+    }
+}
