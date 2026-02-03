0169b9e2a11f3a1774c21f0da4d8d71c267b63d5
tapesh1134 <tapeshs1134@gmail.com>
2026-02-03 15:53:07 +0530

    [tapesh] Refactor. Implement required functionalities

--- design-patterns-practice/gcr-codebase/library-management-system/LibraryManagementSystem.java
+++ design-patterns-practice/gcr-codebase/library-management-system/LibraryManagementSystem.java
@@ -1,4 +1,24 @@
 
+import java.util.Arrays;
+
 public class LibraryManagementSystem {
 
+    public static void main(String[] args) {
+
+        LibraryCatalog catalog = LibraryCatalog.getInstance();
+
+        catalog.notifier().subscribe(new LibraryUser("Tapesh"));
+        catalog.notifier().subscribe(new LibraryUser("Amit"));
+
+        Book book = new Book.Builder("Data Structures by Cormen")
+                .authors(Arrays.asList("Cormen", "Leiserson"))
+                .edition("3rd")
+                .genre("CS")
+                .build();
+
+        catalog.addBook(book);
+
+        User user = UserFactory.create("student");
+        user.role();
+    }
 }
